fb6ea372b3a1f05d138bafca44f60d34eb14648e
Serhii Kolisnyk <kolesnik.serjick@gmail.com>
2022-11-03 19:57:11 +0100

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,5 +2,36 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const gallery = document.querySelector('.gallery');
+const newGallery = createElements(galleryItems);
+gallery.innerHTML = newGallery;
+gallery.addEventListener('click', clickOnImage);
+
+function createElements() {
+  return galleryItems
+    .map(item => {
+      return `<div class ="gallery__item">
+              <a class="gallery__item" href=${item.original}>
+                <img class="gallery__image" 
+                  src=${item.preview} 
+                  alt=${item.description} />
+              </a>;
+              </div>`;
+    })
+    .join('');
+}
+
+function clickOnImage(event) {
+  event.preventDefault();
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionSelector: 'img',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
 console.log(galleryItems);
